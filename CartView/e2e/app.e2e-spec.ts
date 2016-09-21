4399259a11df7e4d8611e470897afa56a74a2b66
capenap <capenap@unal.edu.co>
2016-09-20 20:37:28 -0500

    First

---
+++ CartView/e2e/app.e2e-spec.ts
@@ -0,0 +1,15 @@
+
+describe('Cart Test', function () {
+
+  let expectedMsg = 'Cart Test';
+
+
+  beforeEach(function () {
+    browser.get('');
+  });
+
+  it('should display: ' + expectedMsg, function () {
+    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  });
+
+});
